c404c2da4764d96e945ca19f8327dc27963817e5
Антон <fiery2017@yandex.ru>
2024-02-20 18:02:10 +0300

    Added factorial logic

--- MDK/MathLogics/Factorial.cs
+++ MDK/MathLogics/Factorial.cs
@@ -8,5 +8,14 @@ namespace MDK
 {
     internal class Factorial
     {
+        public static int Calculate (int n)
+        {
+            int res = 1;
+            for (int i = 0; i < n; i++)
+            {
+                res *= 1;
+            }
+            return res;
+        }
     }
 }
