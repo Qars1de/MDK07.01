ec29d465adb1ca3dd25388f249d139642823695a
Антон <fiery2017@yandex.ru>
2024-02-20 18:08:12 +0300

    Added hypotenuse logic

--- MDK/PhysicLogic/PifagorMethod.cs
+++ MDK/PhysicLogic/PifagorMethod.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MDK
 {
     internal class PifagorMethod
     {
+        static double CalculateHypotenuse (int a, int b, int angle)
+        {
+            double c = Math.Pow(a ,2) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angle);
+            return Math.Sqrt(c);
+        }
     }
 }
